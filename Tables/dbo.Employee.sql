09c4b06d351e3e711381986970fc77c1371caffc
rajasrirangan <rajasrirangan@gmail.com>
2016-02-16 18:35:38 +0530

    version 1

--- Tables/dbo.Employee.sql
+++ Tables/dbo.Employee.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Employee]
 [id] [int] NOT NULL IDENTITY(1, 1),
 [EmpName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [EmpAddress] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[EmpEmailId] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[EmpEmailId] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Phone] [varbinary] (50) NULL
 ) ON [PRIMARY]
 GO
