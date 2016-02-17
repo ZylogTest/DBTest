5692291416e37fcd9ed487c96351ae9c2fede984
rajasrirangan <rajasrirangan@gmail.com>
2016-02-17 15:54:31 +0530

    Static Data

---
+++ Data/dbo.Ref_Users_Data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Ref_Users] ON
+INSERT INTO [dbo].[Ref_Users] ([UserId], [UserName], [FirstName], [LastName], [Password], [RoleId], [OrgId], [IsActive], [CreatedDate], [CreatedBy], [UpdatedDate], [UpdatedBy]) VALUES (1, 'RajaSrirangan', 'Raja', 'Srirangan', 'Test123', 1, 1, 1, '2015-02-25 16:15:13.750', 1, NULL, NULL)
+INSERT INTO [dbo].[Ref_Users] ([UserId], [UserName], [FirstName], [LastName], [Password], [RoleId], [OrgId], [IsActive], [CreatedDate], [CreatedBy], [UpdatedDate], [UpdatedBy]) VALUES (2, 'Anchit', 'Anchit', 'T', 'Test123', 1, 1, 1, '2015-02-25 16:15:37.103', 1, NULL, NULL)
+INSERT INTO [dbo].[Ref_Users] ([UserId], [UserName], [FirstName], [LastName], [Password], [RoleId], [OrgId], [IsActive], [CreatedDate], [CreatedBy], [UpdatedDate], [UpdatedBy]) VALUES (3, 'Venkat', 'Venkat', 'R', 'Test123', 1, 1, 1, '2015-02-25 16:15:56.853', 1, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Ref_Users] OFF
