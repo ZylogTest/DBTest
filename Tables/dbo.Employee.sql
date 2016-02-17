0efe89c0938457736e68d10248cdb4f35374eb1c
rajasrirangan <rajasrirangan@gmail.com>
2016-02-17 16:02:50 +0530

    Removed phone column

--- Tables/dbo.Employee.sql
+++ Tables/dbo.Employee.sql
@@ -3,7 +3,6 @@ CREATE TABLE [dbo].[Employee]
 [id] [int] NOT NULL IDENTITY(1, 1),
 [EmpName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [EmpAddress] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[EmpEmailId] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Phone] [varbinary] (50) NULL
+[EmpEmailId] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
